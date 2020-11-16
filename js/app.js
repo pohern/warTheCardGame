f6ba587a6b3f6a07c0f847bb4efd29781f22092f
pohern <ohern_p@yahoo.com>
2020-11-16 12:08:11 -0600

    cached elements

--- js/app.js
+++ js/app.js
@@ -7,7 +7,10 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const message = document.querySelector('.message')
+const buttons = document.querySelectorAll('button')
+const gamePlay = document.querySelector('.gamePlay')
+const userPlay = document.querySelector('.userPlay')
 
 /*----------------------------- Event Listeners -----------------------------*/
 

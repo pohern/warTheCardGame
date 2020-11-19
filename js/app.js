3d45bd899d7d5f862f6d35b441941137a520db11
pohern <ohern_p@yahoo.com>
2020-11-18 23:08:45 -0600

    js works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
-// const ranks = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
-const ranks = [2,3];
+const ranks = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
+// const ranks = [2,3];
 const suits = ['hearts','spades','diams','clubs'];
 
 

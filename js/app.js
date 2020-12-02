fa5644e7d3884a589a56b4822cf2276255a94e51
pohern <ohern_p@yahoo.com>
2020-12-02 09:36:27 -0600

    working on cleaning up ReadME

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
-const ranks = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
-const suits = ['hearts','spades','diams','clubs'];
+const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+const suits = ['hearts', 'spades', 'diams', 'clubs'];
 const quotes = [];
 /*---------------------------- Variables (state) ----------------------------*/
 let deck = [];
@@ -23,33 +23,33 @@ const jokes = document.getElementById('jokesButton');
 const container = document.getElementById('containerDiv');
 message.style.color = 'white';
 /*----------------------------- Event Listeners -----------------------------*/
-buttons.forEach(function(item){
-    item.addEventListener('click',playGame);
+buttons.forEach(function (item) {
+    item.addEventListener('click', playGame);
 });
 
 flipBtn.addEventListener('click', addition);
 
 jokes.addEventListener('click', () => {
     fetch('https://official-joke-api.appspot.com/jokes/programming/random')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data)=> {
-        let newQuote = {}
-        newQuote['artist'] = data[0].setup
-        newQuote['quote'] = data[0].punchline
-        quotes.push(newQuote)
-        render()
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let newQuote = {}
+            newQuote['artist'] = data[0].setup
+            newQuote['quote'] = data[0].punchline
+            quotes.push(newQuote)
+            render()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 });
-jokes.addEventListener('click',function(){
+jokes.addEventListener('click', function () {
     laughing.play()
 });
 /*-------------------------------- Functions --------------------------------*/
-function appendDiv (quote, artist, idx){
+function appendDiv(quote, artist, idx) {
     let newDiv = document.createElement('div')
     newDiv.innerHTML = `
                         <div class="jokeCard h-100" id="${quote.toLowerCase()}">
@@ -73,42 +73,42 @@ function deleteQuote(idx) {
 function render() {
     container.innerHTML = ""
     quotes.forEach((quote, idx) => {
-      appendDiv(quote["quote"], quote["artist"], idx)
+        appendDiv(quote["quote"], quote["artist"], idx)
     })
 };
 
-function addition(){
+function addition() {
     let input2 = totalRounds.value
     result = parseInt(input1.value) + parseInt(input2)
     totalRounds.value = result
 };
 
-function playGame(e){
+function playGame(e) {
     let temp = e.target.textContent;
-    if (temp === 'Start'){
+    if (temp === 'Start') {
         message.style.color = 'black';
         btnToggle();
-        startGame(); 
+        startGame();
     }
-    if (temp == 'Flip'){
+    if (temp == 'Flip') {
         let tempRuns = document.querySelector('input').value;
         responseEl.innerHTML = '';
         round = 0;
-        for (let x = 0; x < tempRuns; x++){
-            if(inPlay){
-                message.innerHTML = `You played ${(x+1)} turns`;
+        for (let x = 0; x < tempRuns; x++) {
+            if (inPlay) {
+                message.innerHTML = `You played ${(x + 1)} turns`;
                 makeCards();
             }
         }
     }
 };
 
-function btnToggle(){
+function btnToggle() {
     buttons[0].classList.toggle('hide');
     buttons[1].classList.toggle('hide');
 };
 
-function startGame(){
+function startGame() {
     inPlay = true;
     gamePlay.innerHTML = '';
     let numberPlayers = document.querySelector('input').value;
@@ -119,176 +119,176 @@ function startGame(){
     document.querySelector('input').value = '1';
 };
 
-function showCard(element,card) {
-    if(card != undefined){
+function showCard(element, card) {
+    if (card != undefined) {
         element.style.backgroundColor = 'rgb(24, 98, 236)';
         let div = document.createElement('div');
         div.classList.add('card');
-        if (card.suit ==='diams'){
+        if (card.suit === 'diams') {
             div.classList.add('diamonds')
-            if (card.rank === 'A'){
+            if (card.rank === 'A') {
                 div.classList.add('A')
             }
-            if (card.rank === 'K'){
+            if (card.rank === 'K') {
                 div.classList.add('K')
             }
-            if (card.rank === 'Q'){
+            if (card.rank === 'Q') {
                 div.classList.add('Q')
             }
-            if (card.rank === 'J'){
+            if (card.rank === 'J') {
                 div.classList.add('J')
             }
-            if (card.rank === 10){
+            if (card.rank === 10) {
                 div.classList.add('r10')
             }
-            if (card.rank === 9){
+            if (card.rank === 9) {
                 div.classList.add('r09')
             }
-            if (card.rank === 8){
+            if (card.rank === 8) {
                 div.classList.add('r08')
             }
-            if (card.rank === 7){
+            if (card.rank === 7) {
                 div.classList.add('r07')
             }
-            if (card.rank === 6){
+            if (card.rank === 6) {
                 div.classList.add('r06')
             }
-            if (card.rank === 5){
+            if (card.rank === 5) {
                 div.classList.add('r05')
             }
-            if (card.rank === 4){
+            if (card.rank === 4) {
                 div.classList.add('r04')
             }
-            if (card.rank === 3){
+            if (card.rank === 3) {
                 div.classList.add('r03')
             }
-            if (card.rank === 2){
+            if (card.rank === 2) {
                 div.classList.add('r02')
             }
         }
-        if (card.suit ==='hearts'){
+        if (card.suit === 'hearts') {
             div.classList.add('hearts')
-            if (card.rank === 'A'){
+            if (card.rank === 'A') {
                 div.classList.add('A')
             }
-            if (card.rank === 'K'){
+            if (card.rank === 'K') {
                 div.classList.add('K')
             }
-            if (card.rank === 'Q'){
+            if (card.rank === 'Q') {
                 div.classList.add('Q')
             }
-            if (card.rank === 'J'){
+            if (card.rank === 'J') {
                 div.classList.add('J')
             }
-            if (card.rank === 10){
+            if (card.rank === 10) {
                 div.classList.add('r10')
             }
-            if (card.rank === 9){
+            if (card.rank === 9) {
                 div.classList.add('r09')
             }
-            if (card.rank === 8){
+            if (card.rank === 8) {
                 div.classList.add('r08')
             }
-            if (card.rank === 7){
+            if (card.rank === 7) {
                 div.classList.add('r07')
             }
-            if (card.rank === 6){
+            if (card.rank === 6) {
                 div.classList.add('r06')
             }
-            if (card.rank === 5){
+            if (card.rank === 5) {
                 div.classList.add('r05')
             }
-            if (card.rank === 4){
+            if (card.rank === 4) {
                 div.classList.add('r04')
             }
-            if (card.rank === 3){
+            if (card.rank === 3) {
                 div.classList.add('r03')
             }
-            if (card.rank === 2){
+            if (card.rank === 2) {
                 div.classList.add('r02')
             }
         }
-        if (card.suit ==='clubs'){
+        if (card.suit === 'clubs') {
             div.classList.add('clubs')
-            if (card.rank === 'A'){
+            if (card.rank === 'A') {
                 div.classList.add('A')
             }
-            if (card.rank === 'K'){
+            if (card.rank === 'K') {
                 div.classList.add('K')
             }
-            if (card.rank === 'Q'){
+            if (card.rank === 'Q') {
                 div.classList.add('Q')
             }
-            if (card.rank === 'J'){
+            if (card.rank === 'J') {
                 div.classList.add('J')
             }
-            if (card.rank === 10){
+            if (card.rank === 10) {
                 div.classList.add('r10')
             }
-            if (card.rank === 9){
+            if (card.rank === 9) {
                 div.classList.add('r09')
             }
-            if (card.rank === 8){
+            if (card.rank === 8) {
                 div.classList.add('r08')
             }
-            if (card.rank === 7){
+            if (card.rank === 7) {
                 div.classList.add('r07')
             }
-            if (card.rank === 6){
+            if (card.rank === 6) {
                 div.classList.add('r06')
             }
-            if (card.rank === 5){
+            if (card.rank === 5) {
                 div.classList.add('r05')
             }
-            if (card.rank === 4){
+            if (card.rank === 4) {
                 div.classList.add('r04')
             }
-            if (card.rank === 3){
+            if (card.rank === 3) {
                 div.classList.add('r03')
             }
-            if (card.rank === 2){
+            if (card.rank === 2) {
                 div.classList.add('r02')
             }
         }
-        if (card.suit ==='spades'){
+        if (card.suit === 'spades') {
             div.classList.add('spades')
-            if (card.rank === 'A'){
+            if (card.rank === 'A') {
                 div.classList.add('A')
             }
-            if (card.rank === 'K'){
+            if (card.rank === 'K') {
                 div.classList.add('K')
             }
-            if (card.rank === 'Q'){
+            if (card.rank === 'Q') {
                 div.classList.add('Q')
             }
-            if (card.rank === 'J'){
+            if (card.rank === 'J') {
                 div.classList.add('J')
             }
-            if (card.rank === 10){
+            if (card.rank === 10) {
                 div.classList.add('r10')
             }
-            if (card.rank === 9){
+            if (card.rank === 9) {
                 div.classList.add('r09')
             }
-            if (card.rank === 8){
+            if (card.rank === 8) {
                 div.classList.add('r08')
             }
-            if (card.rank === 7){
+            if (card.rank === 7) {
                 div.classList.add('r07')
             }
-            if (card.rank === 6){
+            if (card.rank === 6) {
                 div.classList.add('r06')
             }
-            if (card.rank === 5){
+            if (card.rank === 5) {
                 div.classList.add('r05')
             }
-            if (card.rank === 4){
+            if (card.rank === 4) {
                 div.classList.add('r04')
             }
-            if (card.rank === 3){
+            if (card.rank === 3) {
                 div.classList.add('r03')
             }
-            if (card.rank === 2){
+            if (card.rank === 2) {
                 div.classList.add('r02')
             }
         }
@@ -296,23 +296,23 @@ function showCard(element,card) {
     }
 };
 
-function dealRound(playerList, tempHolder){
+function dealRound(playerList, tempHolder) {
     let currentWinner = {
         'high': null,
         'player': playerList[0]
     }
     let war = [];
-    for(let x = 0; x < playerList.length; x++){
+    for (let x = 0; x < playerList.length; x++) {
         let tempPlayerIdx = playerList[x];
-        if(deals[tempPlayerIdx].length > 0){
+        if (deals[tempPlayerIdx].length > 0) {
             let card = deals[tempPlayerIdx].shift();
-            if(currentWinner.high == card.value){
-                if (war.length == 0){
+            if (currentWinner.high == card.value) {
+                if (war.length == 0) {
                     war.push(currentWinner.player);
                 }
                 war.push(tempPlayerIdx);
             }
-            if(!currentWinner.high || currentWinner.high < card.value){
+            if (!currentWinner.high || currentWinner.high < card.value) {
                 war = [];
                 currentWinner.high = card.value;
                 currentWinner.player = tempPlayerIdx;
@@ -320,7 +320,8 @@ function dealRound(playerList, tempHolder){
             }
             tempHolder.push(card);
             showCard(players[tempPlayerIdx], card);
-    }}
+        }
+    }
     if (war.length > 0) {
         dealRound(war, tempHolder);
     } else {
@@ -328,26 +329,26 @@ function dealRound(playerList, tempHolder){
     }
 };
 
-function makeCards(){
+function makeCards() {
     let tempHolder = [];
     let playerList = [];
-    for(let x = 0; x < players.length; x++){
+    for (let x = 0; x < players.length; x++) {
         players[x].innerHTML = '';
-        if (deals[x].length > 0){
+        if (deals[x].length > 0) {
             playerList.push(x);
         }
     }
-    if (playerList.length == 1){
+    if (playerList.length == 1) {
         winGame();
     }
-    dealRound(playerList,tempHolder);
+    dealRound(playerList, tempHolder);
 };
 
-function winGame(){
+function winGame() {
     message.style.color = 'red';
     btnToggle();
     inPlay = false;
-    for(let x = 0; x < players.length; x++){
+    for (let x = 0; x < players.length; x++) {
         players[x].innerHTML += (deals[x].length >= total) ? `<br>WINNER` : `<br>LOSER`;
     }
     confetti.start(3000);
@@ -356,35 +357,35 @@ function winGame(){
     totalRounds.value = '-1';
 };
 
-function updater(winner, tempHolder){
+function updater(winner, tempHolder) {
     players[winner].style.backgroundColor = 'green'
-    players.forEach(player => 
-        player.setAttribute('class','')
+    players.forEach(player =>
+        player.setAttribute('class', '')
     );
-    players[winner].setAttribute('class','animate__animated animate__tada');
-    tempHolder.sort(function(){
+    players[winner].setAttribute('class', 'animate__animated animate__tada');
+    tempHolder.sort(function () {
         return .5 - Math.random();
     })
-    for(let record of tempHolder){
+    for (let record of tempHolder) {
         deals[winner].push(record);
     }
-    for(let x = 0; x < players.length; x++){
+    for (let x = 0; x < players.length; x++) {
         let div = document.createElement('div');
         div.classList.add('stats')
-        if( deals[x].length == total){
+        if (deals[x].length == total) {
             div.innerHTML = `Total ${deals[x].length} cards`;
             winGame();
         } else {
             div.innerHTML = deals[x].length < 1 ? 'Lost' : `Cards: ${(deals[x].length)}`;
         }
-        players[x].appendChild(div);   
+        players[x].appendChild(div);
     }
     responseEl.innerHTML += `Player ${(winner + 1)} won ${tempHolder.length} cards<br>`;
 };
 
-function dealCards(playerCard){
+function dealCards(playerCard) {
     playerCard = (playerCard >= players.length) ? 0 : playerCard;
-    if(deck.length > 0){
+    if (deck.length > 0) {
         let randIndex = Math.floor(Math.random() * deck.length);
         let card = deck.splice(randIndex, 1)[0];
         deals[playerCard].push(card);
@@ -396,10 +397,10 @@ function dealCards(playerCard){
     }
 };
 
-function buildDeck(){
+function buildDeck() {
     deck = [];
-    for(let i = 0; i < suits.length; i++){
-        for (let j = 0; j < ranks.length; j++){
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < ranks.length; j++) {
             let card = {};
             total++;
             card.suit = suits[i];
@@ -410,15 +411,15 @@ function buildDeck(){
     }
 };
 
-function setupPlayers(num){
+function setupPlayers(num) {
     players = [];
     deals = [];
-    for(let x = 0; x < num; x++){
+    for (let x = 0; x < num; x++) {
         let div = document.createElement('div');
-        div.setAttribute('id','player'+(x+1));
+        div.setAttribute('id', 'player' + (x + 1));
         div.classList.add('player');
         let div1 = document.createElement('div');
-        div1.textContent = `Player ${parseInt((x)+1)}`;
+        div1.textContent = `Player ${parseInt((x) + 1)}`;
         players[x] = document.createElement('div');
         players[x].textContent = 'Cards';
         div.appendChild(div1);
